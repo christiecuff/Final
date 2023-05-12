c5430264ace05cda7e7243f25376b7d0bf190369
christiecuff <112016251+christiecuff@users.noreply.github.com>
2023-05-12 03:45:56 +0000

    add that begining part

---
+++ Finel.py
@@ -0,0 +1,17 @@
+print("\tMan's Homocide\n")
+name = input(f'State your name:\n')
+
+print('''Note: You are a detective investigating 
+a crime that was commited 1 hour ago at the Henderson 
+house hold. You have 7 supects.
+''')
+input('\t\t\t\t\tEnter to continue')
+
+print('''Note: The victim's name was Josh Henderson. 
+32 years old, a lawyer, he was found dead on the first floor in his study. 
+The latest suspects: Millie Parker (maid), Georgia Valintine (maid), 
+Amina Bradford (maid), Daisie Robies (cook), Elliot Yang (cook), 
+Kelsey Henderson (wife) and Alfred Conley (butler)
+''')
+input('\t\t\t\t\t\tEnter to continue')
+
